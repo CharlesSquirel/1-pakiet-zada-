2e44caa65b3daf524e3ab15bb86641d1f02b5c26
Karol Wiewiórka <karol.wiewiorka@gmail.com>
2023-01-06 21:57:47 +0100

    JS

--- 5/script.js
+++ 5/script.js
@@ -12,9 +12,20 @@ const createForm = document.querySelector(".create-form");
 const personalForm = document.querySelector(".personal-form");
 const socialForm = document.querySelector(".social-form");
 
+// NEXT CREATE BUTTON EVENT
 nextCreateButton.addEventListener("click", () => {
     createForm.style.left = "-580px";
-    personalForm.style.left = "0";
+    socialForm.style.left = "0";
+    personalForm.style.left = "580px"
     progressBar.style.width = "475px";
     progressArrow.style.left = "456px";
+})
+
+// BACK PERSONAL BUTTON EVENT
+backPersonalButton.addEventListener("click", (e) => {
+    createForm.style.left = "0";
+    socialForm.style.left = "580px";
+    personalForm.style.left = "1160px"
+    progressBar.style.width = "255px";
+    progressArrow.style.left = "234px";
 })
