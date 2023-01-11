4a79d98f91a59502c1d51fe44be501704562e0af
Karol Wiewiórka <karol.wiewiorka@gmail.com>
2023-01-11 14:46:41 +0100

    JS RWD

--- 5/script.js
+++ 5/script.js
@@ -14,30 +14,57 @@ const personalForm = document.querySelector(".personal-form");
 const socialForm = document.querySelector(".social-form");
 // BUTTON GROUP OF THE SAME EVENT
 const buttonGroup = [].concat(nextCreateButton, backPersonalButton);
+// WATCH MEDIA
+const mobile = window.matchMedia("(max-width: 687px)");
+// INPUTS
+const emailInput = document.querySelector("#email");
+
+// // VALIDATION
+// nextCreateButton.addEventListener("click", (e) => {
+//   if (emailInput.length === 0) {
+//     alert("dfgdfgd");
+//     e.stopImmediatePropagation();
+//   }
+// });
 
 // // NEXT CREATE BUTTON EVENT && BACK PERSONAL BUTTON EVENT
-buttonGroup.forEach(button => button.addEventListener("click", () => {
+buttonGroup.forEach((button) =>
+  button.addEventListener("click", () => {
     createForm.style.left = "-580px";
     socialForm.style.left = "0";
-    personalForm.style.left = "580px"
-    progressBar.style.width = "475px";
+    personalForm.style.left = "580px";
     stepText2.style.fontWeight = "600";
-}));
-
+    stepText3.style.fontWeight = "400";
+    if (mobile.matches) {
+      progressBar.style.width = "260px";
+    } else {
+      progressBar.style.width = "475px";
+    }
+  })
+);
 
 // BACK SOCIAL BUTTON EVENT
 backSocialButton.addEventListener("click", (e) => {
-    createForm.style.left = "0";
-    socialForm.style.left = "580px";
-    personalForm.style.left = "1160px"
+  createForm.style.left = "0";
+  socialForm.style.left = "580px";
+  personalForm.style.left = "1160px";
+  stepText2.style.fontWeight = "400";
+  if (mobile.matches) {
+    progressBar.style.width = "130px";
+  } else {
     progressBar.style.width = "255px";
+  }
 });
 
 // NEXT SOCIAL BUTTON EVENT
 nextSocialButton.addEventListener("click", () => {
-    createForm.style.left = "-1160px";
-    socialForm.style.left = "-580px";
-    personalForm.style.left = "0"
+  createForm.style.left = "-1160px";
+  socialForm.style.left = "-580px";
+  personalForm.style.left = "0";
+  stepText3.style.fontWeight = "600";
+  if (mobile.matches) {
+    progressBar.style.width = "500px";
+  } else {
     progressBar.style.width = "650px";
-    stepText3.style.fontWeight = "600";
+  }
 });
