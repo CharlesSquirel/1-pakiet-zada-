0144d3aafb4eea445668109304629c6e44e00761
Karol Wiewiórka <karol.wiewiorka@gmail.com>
2023-01-11 15:52:56 +0100

    FIRST APPROACH header with hamburger events

---
+++ 3/script.js
@@ -0,0 +1,13 @@
+// DOM
+const hamburger = document.querySelector(".hamburger");
+const closeBtn = document.querySelector(".close-btn");
+const navMobile = document.querySelector(".nav-top-mobile");
+
+// EVENTS HAMBURGER
+hamburger.addEventListener("click", () => {
+  navMobile.style.left = "0";
+});
+
+closeBtn.addEventListener("click", () => {
+  navMobile.style.left = "-400px";
+});
