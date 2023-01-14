0ea9b2948b4eaa4c93d0ecddac4be62b0652dc06
Karol Wiewiórka <karol.wiewiorka@gmail.com>
2023-01-14 14:27:44 +0100

    Funkcje w oddzielnych zmiennych

--- 5/script.js
+++ 5/script.js
@@ -12,39 +12,29 @@ const progressBar = document.querySelector(".progress");
 const createForm = document.querySelector(".create-form");
 const personalForm = document.querySelector(".personal-form");
 const socialForm = document.querySelector(".social-form");
-// BUTTON GROUP OF THE SAME EVENT
+const forms = document.querySelectorAll("form");
+const allButtons = document.querySelectorAll(".btn");
 const buttonGroup = [].concat(nextCreateButton, backPersonalButton);
 // WATCH MEDIA
 const mobile = window.matchMedia("(max-width: 687px)");
 // INPUTS
 const emailInput = document.querySelector("#email");
 
-// // VALIDATION
-// nextCreateButton.addEventListener("click", (e) => {
-//   if (emailInput.length === 0) {
-//     alert("dfgdfgd");
-//     e.stopImmediatePropagation();
-//   }
-// });
+//FUNCTIONS NEXT/BACK
+const nextCreate = () => {
+  createForm.style.left = "-580px";
+  socialForm.style.left = "0";
+  personalForm.style.left = "580px";
+  stepText2.style.fontWeight = "600";
+  stepText3.style.fontWeight = "400";
+  if (mobile.matches) {
+    progressBar.style.width = "260px";
+  } else {
+    progressBar.style.width = "475px";
+  }
+};
 
-// // NEXT CREATE BUTTON EVENT && BACK PERSONAL BUTTON EVENT
-buttonGroup.forEach((button) =>
-  button.addEventListener("click", () => {
-    createForm.style.left = "-580px";
-    socialForm.style.left = "0";
-    personalForm.style.left = "580px";
-    stepText2.style.fontWeight = "600";
-    stepText3.style.fontWeight = "400";
-    if (mobile.matches) {
-      progressBar.style.width = "260px";
-    } else {
-      progressBar.style.width = "475px";
-    }
-  })
-);
-
-// BACK SOCIAL BUTTON EVENT
-backSocialButton.addEventListener("click", (e) => {
+const backSocial = () => {
   createForm.style.left = "0";
   socialForm.style.left = "580px";
   personalForm.style.left = "1160px";
@@ -54,10 +44,9 @@ backSocialButton.addEventListener("click", (e) => {
   } else {
     progressBar.style.width = "255px";
   }
-});
+};
 
-// NEXT SOCIAL BUTTON EVENT
-nextSocialButton.addEventListener("click", () => {
+const nextSocial = () => {
   createForm.style.left = "-1160px";
   socialForm.style.left = "-580px";
   personalForm.style.left = "0";
@@ -67,4 +56,19 @@ nextSocialButton.addEventListener("click", () => {
   } else {
     progressBar.style.width = "650px";
   }
-});
+};
+
+// DISABLED FOR ALL SUBMIT BUTTONS
+// allButtons.forEach((btn) => {
+//   btn.setAttribute("disabled", "");
+// });
+
+// FORMS EVENTS
+// NEXT CREATE BUTTON EVENT && BACK PERSONAL BUTTON EVENT
+buttonGroup.forEach((button) => button.addEventListener("click", nextCreate));
+
+// BACK SOCIAL BUTTON EVENT
+backSocialButton.addEventListener("click", backSocial);
+
+// NEXT SOCIAL BUTTON EVENT
+nextSocialButton.addEventListener("click", nextSocial);
